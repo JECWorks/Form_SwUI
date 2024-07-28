9f44fe582006108d2b6bea7c5abf2051b4abc700
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-28 18:46:57 -0400

    Initial Commit

---
+++ Form_SwUI/Form_SwUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_SwUIApp.swift
+//  Form_SwUI
+//
+//  Created by Jason Cox on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Form_SwUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
