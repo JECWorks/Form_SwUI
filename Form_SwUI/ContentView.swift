072c5b087d4ecd8d9daf251a755415540a404970
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-28 20:01:19 -0400

    Initial commit of general usage "form" in SwiftUI. Very useful!

--- Form_SwUI/ContentView.swift
+++ Form_SwUI/ContentView.swift
@@ -8,17 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var shouldSendNewsletter = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("Personal Information")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                }
+                Section(header: Text("Actions")) {
+                    Toggle("Send newsletter", isOn: $shouldSendNewsletter)
+                        .toggleStyle(SwitchToggleStyle(tint: .green))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms of Service", destination: URL(string: "https://seanallen.co")!)
+                }
+            }
+            
+            .navigationTitle("Account")
+//            .onTapGesture {
+//                hideKeyboard()
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        hideKeyboard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    Button("Save", action: saveUser)
+                }
+            }
         }
         .padding()
+        .accentColor(.orange)
+        
+    }
+    func saveUser() {
+        print("User Saved")
     }
 }
 
 #Preview {
     ContentView()
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+// This extension will allow the keyboard to be dismissed after it is brought up.
